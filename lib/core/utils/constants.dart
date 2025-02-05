734f5b25427323f674e60dd8b6b81b77a8dc61c1
suleymansurucu <suleymansurucu95@gmail.com>
2025-02-05 17:12:59 -0500

    feat : create constant page

---
+++ lib/core/utils/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Constants{
+  static const Color background = Color(0xFFF8F9FA); // White for Background
+  static const Color primary = Color(0xFF264653); // Navy Blue (Primary Color)
+  static const Color accent = Color(0xFF2A9D8F); // Green (For Button Color)
+  static const Color highlight = Color(0xFFE9C46A); // Yellow (for Highlights )
+  static const Color textColor = Color(0xFF1D3557); // Grey (For Text)
+
+}
