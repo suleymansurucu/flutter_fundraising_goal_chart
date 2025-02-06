cbf9a17eb4de66fb6612ce33f08536f78dca11f9
suleymansurucu <suleymansurucu95@gmail.com>
2025-02-05 19:15:14 -0500

    feat : create UI for forgot page

---
+++ lib/views/auth/forgot_password.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_fundraising_goal_chart/core/utils/constants.dart';
+import 'package:flutter_fundraising_goal_chart/core/utils/constants/build_Text_Form_Field.dart';
+import 'package:flutter_fundraising_goal_chart/core/utils/constants/build_elevated_button.dart';
+import 'package:flutter_fundraising_goal_chart/core/utils/constants/build_text_for_form.dart';
+
+class ForgotPasswordPage extends StatefulWidget {
+  const ForgotPasswordPage({super.key});
+
+  @override
+  State<ForgotPasswordPage> createState() => _ForgotPasswordPageState();
+}
+
+class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
+  final TextEditingController _emailController=TextEditingController();
+
+  void _forgotPassword(){
+    print('Your Email Adress ${_emailController.text}');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Constants.background,
+      body: Center(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(16),
+          child: Container(
+            width: 400,
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(15),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.shade200,
+                  spreadRadius: 3,
+                  blurRadius: 7,
+                  offset: Offset(0, 3),
+                ),
+              ],
+            ),
+            child: Form(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  SizedBox(height: 20),
+                  Align(
+                    child: Text(
+                      "Forgot Password",
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                        color: Constants.textColor,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  BuildTextForForm(
+                      text: 'Please Enter Your Email Address:',
+                      textColor: Constants.textColor,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w100),
+                  SizedBox(height: 4),
+                  BuildTextFormField(
+                      keyBoardType: TextInputType.emailAddress,
+                      labetText: 'Email',
+                      hintText: 'Enter Email Address',
+                      textFormFieldIcon: Icons.email,
+                      textFormFieldIconColor: Constants.accent,
+                      outLineInputBorderColor: Constants.accent,
+                      outLineInputBorderColorOnFocused: Constants.primary,
+                      textEditingController: _emailController),
+                  SizedBox(height: 20),
+                  BuildElevatedButton(
+                    onPressed: _forgotPassword,
+                    buttonText: "Forgot",
+                    buttonColor: Constants.accent,
+                    textColor: Colors.white,
+                    borderRadius: 15,
+                    paddingHorizontal: 50,
+                    paddingVertical: 20,
+                    iconColor: Colors.white,
+                    hasShadow: true,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
