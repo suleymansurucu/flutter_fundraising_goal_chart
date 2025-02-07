27448ecfba7cd6182b5969a4e5a794abff01060b
suleymansurucu <suleymansurucu95@gmail.com>
2025-02-06 23:41:21 -0500

    feat : have integrated firabase Auth signInWithEmailAndPassword

--- lib/views/auth/sing_in.dart
+++ lib/views/auth/sing_in.dart
@@ -1,10 +1,15 @@
+import 'package:another_flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_fundraising_goal_chart/core/utils/constants.dart';
 import 'package:flutter_fundraising_goal_chart/core/utils/constants/build_Text_Form_Field.dart';
 import 'package:flutter_fundraising_goal_chart/core/utils/constants/build_elevated_button.dart';
 import 'package:flutter_fundraising_goal_chart/core/utils/constants/build_text_for_form.dart';
+import 'package:flutter_fundraising_goal_chart/models/user_model.dart';
+import 'package:flutter_fundraising_goal_chart/view_models/user_view_models.dart';
 import 'package:flutter_fundraising_goal_chart/views/auth/forgot_password.dart';
 import 'package:flutter_fundraising_goal_chart/views/auth/sign_up.dart';
+import 'package:flutter_fundraising_goal_chart/views/fundraising/fundraising_setup_page.dart';
+import 'package:provider/provider.dart';
 
 class SingInPage extends StatefulWidget {
   const SingInPage({super.key});
@@ -16,14 +21,46 @@ class SingInPage extends StatefulWidget {
 class _SingInPageState extends State<SingInPage> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  late String email, password;
+  final _formKey = GlobalKey<FormState>();
 
-  void _singIn() {
+  void _singIn() async{
+    final UserViewModels userViewModels =
+    Provider.of<UserViewModels>(context, listen: false);
+    bool validateForm=userViewModels.validateFormForSignIn(_emailController, _passwordController);
+    if (validateForm) {
+      _formKey.currentState!.save();
+
+      UserModel? user=await userViewModels.signInWithEmailAndPassword(email, password);
+      if (user != null) {
+        await Flushbar(
+          title: 'Welcome ${user.fullName}',
+          message: 'Your account signed successfully! Thank You!',
+          duration: Duration(seconds: 3),
+          titleColor: Constants.background,
+          messageColor: Constants.background,
+          backgroundColor: Constants.primary,
+          maxWidth: 600,
+          titleSize: 32,
+          showProgressIndicator: true,
+          flushbarPosition: FlushbarPosition.TOP,
+          margin: EdgeInsets.all(20),
+        ).show(context);
+
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => FundraisingSetupPage()));
+      }
+
+      debugPrint(user!.userID);
+
+    }
     debugPrint('Email: ${_emailController.text}');
     debugPrint('Password: ${_passwordController.text}');
   }
-  void _signUp(){
-    Navigator.push(context, MaterialPageRoute(builder: (context)=>SignUpPage()));
 
+  void _signUp() {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => SignUpPage()));
   }
 
   @override
@@ -35,6 +72,8 @@ class _SingInPageState extends State<SingInPage> {
 
   @override
   Widget build(BuildContext context) {
+    final UserViewModels userViewModels = Provider.of<UserViewModels>(
+        context, listen: false);
     return Scaffold(
       backgroundColor: Constants.background,
       body: Center(
@@ -55,30 +94,32 @@ class _SingInPageState extends State<SingInPage> {
                 ),
               ],
             ),
-            child: Form(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  SizedBox(height: 20),
-                  Align(
-                    child: Text(
-                      "Sign In",
-                      style: TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.bold,
-                        color: Constants.textColor,
+            child: Consumer(builder: (context, UserViewModels, child) {
+              return Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    SizedBox(height: 20),
+                    Align(
+                      child: Text(
+                        "Sign In",
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Constants.textColor,
+                        ),
+                        textAlign: TextAlign.center,
                       ),
-                      textAlign: TextAlign.center,
                     ),
-                  ),
-                  SizedBox(height: 20),
-                  BuildTextForForm(
-                      text: 'Please Enter Your Email Address:',
-                      textColor: Constants.textColor,
-                      fontSize: 14,
-                      fontWeight: FontWeight.w100),
-                  SizedBox(height: 4),
-                  BuildTextFormField(
+                    SizedBox(height: 20),
+                    BuildTextForForm(
+                        text: 'Please Enter Your Email Address:',
+                        textColor: Constants.textColor,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w100),
+                    SizedBox(height: 4),
+                    BuildTextFormField(
                       keyBoardType: TextInputType.emailAddress,
                       labelText: 'Email',
                       hintText: 'Enter Email Address',
@@ -86,15 +127,20 @@ class _SingInPageState extends State<SingInPage> {
                       textFormFieldIconColor: Constants.accent,
                       outLineInputBorderColor: Constants.accent,
                       outLineInputBorderColorOnFocused: Constants.primary,
-                      textEditingController: _emailController),
-                  SizedBox(height: 15),
-                  BuildTextForForm(
-                      text: 'Please Enter Password:',
-                      textColor: Constants.textColor,
-                      fontSize: 14,
-                      fontWeight: FontWeight.w100),
-                  SizedBox(height: 4),
-                  BuildTextFormField(
+                      textEditingController: _emailController,
+                      errorText: userViewModels.emailError,
+                      onSaved: (value) {
+                        email = value!;
+                      },
+                    ),
+                    SizedBox(height: 15),
+                    BuildTextForForm(
+                        text: 'Please Enter Password:',
+                        textColor: Constants.textColor,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w100),
+                    SizedBox(height: 4),
+                    BuildTextFormField(
                       keyBoardType: TextInputType.visiblePassword,
                       labelText: 'Password',
                       hintText: 'Enter Password',
@@ -102,58 +148,67 @@ class _SingInPageState extends State<SingInPage> {
                       textFormFieldIconColor: Constants.accent,
                       outLineInputBorderColor: Constants.accent,
                       outLineInputBorderColorOnFocused: Constants.primary,
-                      textEditingController: _passwordController),
-                  SizedBox(height: 20),
-                  BuildElevatedButton(
-                    onPressed: _singIn,
-                    buttonText: "Create",
-                    buttonColor: Constants.accent,
-                    textColor: Colors.white,
-                    borderRadius: 15,
-                    paddingHorizontal: 50,
-                    paddingVertical: 20,
-                    iconColor: Colors.white,
-                    hasShadow: true,
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Opacity(
-                    opacity: 0.64,
-                    child: TextButton(
-                      onPressed: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (context)=>ForgotPasswordPage()));
-
-                      },
-                      child: Text(
-                        'Forgot Password?',
-                        style: TextStyle(color: Constants.primary),
-                      ),
+                      textEditingController: _passwordController,
+                      errorText: userViewModels.passwordError,
+                      onSaved: (value) {
+                        password = value!;
+                      },),
+                    SizedBox(height: 20),
+                    BuildElevatedButton(
+                      onPressed: _singIn,
+                      buttonText: "Create",
+                      buttonColor: Constants.accent,
+                      textColor: Colors.white,
+                      borderRadius: 15,
+                      paddingHorizontal: 50,
+                      paddingVertical: 20,
+                      iconColor: Colors.white,
+                      hasShadow: true,
                     ),
-                  ),
-                  Opacity(
-                    opacity: 0.64,
-                    child: TextButton(
-                      onPressed: () {
-                        _signUp();
-                      },
-                      child: Text.rich(
-                        const TextSpan(
-                          text: "Don’t have an account? ",
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Opacity(
+                      opacity: 0.64,
+                      child: TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => ForgotPasswordPage()));
+                        },
+                        child: Text(
+                          'Forgot Password?',
                           style: TextStyle(color: Constants.primary),
-                          children: [
-                            TextSpan(
-                              text: "Sign Up",
-                              style: TextStyle(color: Constants.accent, fontWeight: FontWeight.bold),
-                            ),
-                          ],
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ),
+                    Opacity(
+                      opacity: 0.64,
+                      child: TextButton(
+                        onPressed: () {
+                          _signUp();
+                        },
+                        child: Text.rich(
+                          const TextSpan(
+                            text: "Don’t have an account? ",
+                            style: TextStyle(color: Constants.primary),
+                            children: [
+                              TextSpan(
+                                text: "Sign Up",
+                                style: TextStyle(
+                                    color: Constants.accent,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            }),
           ),
         ),
       ),
